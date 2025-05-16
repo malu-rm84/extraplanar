e64c09b8eb3805ad7129ddc2680502edf677e02a
malubis <romani.malu84@gmail.com>
2025-05-15 22:34:04 -0300

    types

--- src/components/personagem/types.ts
+++ src/components/personagem/types.ts
@@ -1,11 +1,29 @@
+import { Plano } from "../../data/PlanosRacas";
+import { FaixaEtaria } from "../../data/FaixaEtaria";
+import { Lingua } from "../../data/Linguas";
+import { Experiencia, CategoriaExperiencia } from "../../data/Experiencia";
+import { Pericia, CategoriaPericias } from "../../data/Pericias";
+import { Ocupacao, CategoriaOcupacoes } from "../../data/Ocupacoes";
+import { Capacidade, CategoriaCapacidades } from "../../data/Capacidades";
 
-export type EtapaCriacao = 'dados' | 'atributos' | 'magias' | 'pericias' | 'ocupacoes' | 'capacidades' | 'inventario' | 'extras';
+export type EtapaCriacao =
+  | "dados"
+  | "atributos"
+  | "magias"
+  | "pericias"
+  | "ocupacoes"
+  | "capacidades"
+  | "inventario"
+  | "extras";
 
 export interface Personagem {
   nome: string;
   idade: string;
   plano: string;
   raca: string;
+  faixaEtaria?: FaixaEtaria;
+  linguaMaterna?: Lingua;
+  dialectos?: Lingua[];
   atributos: {
     forca: number;
     destreza: number;
@@ -14,6 +32,13 @@ export interface Personagem {
     sabedoria: number;
     carisma: number;
   };
-  afinidades: any[];
+  afinidades: string[];
   magias: any[];
+  experiencia?: CategoriaExperiencia[];
+  pericias?: CategoriaPericias[];
+  ocupacoes?: CategoriaOcupacoes[];
+  capacidades?: CategoriaCapacidades[];
+  inventario?: any[];
+  extras?: Record<string, any>;
 }
+
